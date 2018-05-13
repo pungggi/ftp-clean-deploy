fedb7763fe68e71869d4a51cf3247e73e571d925
pungggi <alessandro.pungitore@gmail.com>
2018-05-13 13:06:49 +0200

    phase1, Delete Remote files if not existing locally

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const ftp = require('ftp')
-const klawSync = require('klaw-sync')
+const path = require('path')
 
 class FtpClient {
   constructor(
@@ -44,33 +44,81 @@ class FtpClient {
 
   _ready(){
     
-    let pathsLocal
-    try {
-      pathsLocal = klawSync(this.config.localRoot)
-    } catch (er) {
-      console.error(er)
-    }
-    console.dir(pathsLocal)
-
-    this.verbose && console.log('Ready..')
     this.Ftp.cwd(this.config.remoteRoot, (error) => {
       if(error){
         this._error(error)
       }
     })
-
     
+    this.verbose && console.log('Ready..')
 
-    this.Ftp.list( (error, list) => {
+    const isLocalDirectory = source => fs.lstatSync(source).isDirectory()
+    const isLocalFile = source => fs.lstatSync(source).isFile()
+    const getLocalDirectories = source => fs.readdirSync(source).map(name => path.join(source, name)).filter(isLocalDirectory)
+    const getLocalFiles = source => fs.readdirSync(source).map(name => path.join(source, name)).filter(isLocalFile).map(transformForComparison)
+    const transformForComparison = source => {
+      const stat = fs.statSync(source)
+      return formatForComparison(source.split(path.sep).pop(), new Date(
+        stat.mtime.getUTCFullYear(),
+        stat.mtime.getUTCMonth(),
+        stat.mtime.getUTCDate(),
+        stat.mtime.getUTCHours(),
+        stat.mtime.getUTCMinutes(),
+        stat.mtime.getUTCSeconds(), 
+        stat.mtime.getUTCMilliseconds()
+    ), stat.size) 
+    }
+
+    const formatForComparison = (name, date = new Date(), size) => `${name};${date+size}`
+
+    const isRemoteDirectory = source => source.type === 'd' && source.name != '.' && source.name != '..'
+    const isRemoteFile = source => source.type === '-'
+    const getRemoteDirectories = source => source.filter(isRemoteDirectory)
+    const getRemoteFiles = source => source.filter(isRemoteFile).map(file => formatForComparison(file.name, 
+      new Date(
+        file.date.getUTCFullYear(),
+        file.date.getUTCMonth(),
+        file.date.getUTCDate(),
+        file.date.getUTCHours(),
+        file.date.getUTCMinutes(),
+        file.date.getUTCSeconds(), 
+        file.date.getUTCMilliseconds()
+    ), file.size))
+    
+    
+    this.Ftp.list( (error, listRemote) => {
       if(error){
         this._error(error)
       }
-      list.forEach(element => {
-        console.log(element.name)
-      })
-    })
+        this.Ftp.lastMod(this.config.remoteRoot+'/'+listRemote[2].name, (error, lastMod) => {
+          if(error){
+            this._error(error)
+          }
+          let dirRemote = this.config.remoteRoot
+          // console.dir(getLocalDirectories(this.config.localRoot))
+          // console.dir(getRemoteDirectories(list))
+          
+          const filesLocal = getLocalFiles(this.config.localRoot)
+          const filesRemote = getRemoteFiles(listRemote)
 
-    this.Ftp.end()
+          console.dir(filesLocal)
+          console.dir(filesRemote)
+          filesRemote.map(item => {
+            console.log(item)
+            console.log(filesLocal.includes(item))
+            // this.Ftp.end()
+            !filesLocal.includes(item) && this.Ftp.delete(dirRemote+'/'+item.split(';')[0], (error) => {
+              if(error){
+                this._error(error)
+              }
+              console.log('Deleted : '+item.split(';')[0])
+            }) 
+          })
+
+          this.Ftp.end()
+        })
+      }   
+    )
   }
 
   _error(message) {
